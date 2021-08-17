b83645ef532f2dcb569875d3fde20d22a87f2d96
Georgi Arnaudov <georgearnaudov1@gmail.com>
2021-08-17 23:16:38 +0300

    Basic setup

---
+++ src/core/apollo.h
@@ -0,0 +1,10 @@
+#ifndef APOLLO_CORE_APOLLO_H
+#define APOLLO_CORE_APOLLO_H
+
+#include <iostream>
+
+namespace apollo {
+					
+}
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "apollo.h" 
+
+using namespace apollo;
+
+int main() {
+	std::cout << "Welcome to Apollo Renderer!\n";
+	return 0;
+}
