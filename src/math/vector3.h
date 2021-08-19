fe89aeb9012e2669131492cdfe9d4da3f856d6ca
Georgi Arnaudov <georgearnaudov1@gmail.com>
2021-08-19 11:22:41 +0300

    Add 3D vector support

---
+++ src/math/vector3.h
@@ -0,0 +1,191 @@
+#ifndef APOLLO_MATH_VECTOR_3_H
+#define APOLLO_MATH_VECTOR_3_H
+
+#include "apollo.h"
+
+namespace apollo {
+
+template<class T> class Vector3 {
+	public:
+		Vector3() : x(0), y(0), z(0) {}
+		Vector3(T u) : x(u), y(u), z(u) {}
+		Vector3(T xx, T yy, T zz) : x(xx), y(yy), z(zz) {}
+
+		// Copy constructor
+		Vector3(const Vector3<T> &v) {
+			x = v.x;
+			y = v.y;
+			z = v.z;
+		}
+		
+		// Assignment operator
+		Vector3<T>& operator=(const Vector3<T> &v) {
+			x = v.x;
+			y = v.y;
+			z = v.z;
+			return *this;
+		}
+		
+		// Get by index
+		T  operator[](int i) const {
+			if (i == 0)
+				return x;
+			if (i == 1)
+				return y;
+			return z;
+		}
+		
+		T& operator[](int i) {
+			if (i == 0)
+				return x;
+			if (i == 1)
+				return y;
+			return z;
+		}
+
+		// Min and Max components
+		T minComponent() const {
+			if (x < y && x < z)
+				return x;
+			if (y < x && y < z)
+				return y;
+			return z;
+		}
+
+		T maxComponent() const {
+			if (x > y && x > z)
+				return x;
+			if (y > x && y > z)
+				return y;
+			return z;
+		}
+		
+		// Addition and subtraction
+		Vector3<T> operator+(const Vector3<T> &v) const {
+			return Vector3<T>(v.x+x, v.y+y, v.z+z);
+		}
+
+		Vector3<T>& operator+=(const Vector3<T> &v) {
+			x += v.x;
+			y += v.y;
+			z += v.z;
+			return *this;
+		}
+
+		Vector3<T> operator-(const Vector3<T> &v) const {
+			return Vector3<T>(x-v.x, y-v.y, z-v.z);
+		}
+
+		Vector3<T>& operator-=(const Vector3<T> &v) {
+			x -= v.x;
+			y -= v.y;
+			z -= v.z;
+			return *this;
+		}
+
+		// Scalar multiplication and division
+		Vector3<T>  operator*(const T s) const {
+			return Vector3<T>(x*s, y*s, z*s);
+		}
+
+		Vector3<T>& operator*=(const T s) {
+			x *= s;
+			y *= s;
+			z *= s;
+			return *this;
+		}
+
+		Vector3<T>  operator/(const T s) const {
+			float inv = 1.0 / s;
+			return Vector3<T>(x*inv, y*inv, z*inv);
+		}
+
+		Vector3<T>& operator/=(const T s) {
+			float inv = 1.0 / s;
+			x *= inv;
+			y *= inv;
+			z *= inv;
+			return *this;
+		}
+
+		// Vector negation
+		Vector3<T> operator-() const {
+			return Vector3<T>(-x, -y, -z);
+		}
+
+		// Length
+		float lengthSquared() const {
+			return x*x + y*y + z*z; 
+		}
+
+		float length() const {
+			return std::sqrt(lengthSquared());
+		}
+
+		// Absolute value
+		Vector3<T> abs() const {
+			return Vector3<T>(std::abs(x), std::abs(y), std::abs(z));
+		}
+
+		// Normalization
+		Vector3<T>  normalized() const {
+			float invLength = 1.0 / length();
+			return Vector3<T>(x * invLength, y * invLength, z * invLength);
+		}
+
+		Vector3<T>& normalize() {
+			float invLength = 1.0 / length();
+			x *= invLength;
+			y *= invLength;
+			z *= invLength;
+			return *this;
+		}
+
+		// Vector3 public data
+		T x, y, z;
+
+};
+
+// Print
+template <typename T> inline std::ostream& operator<<(std::ostream &out, const Vector3<T> &v) {
+	return out << v.x << ' ' << v.y << ' ' << v.z << '\n';
+}
+
+// Scalar multiplication in case of s * v
+template <typename T> inline Vector3<T> operator*(T s, const Vector3<T> &v) {
+	return v * s;
+}
+
+// Dot product
+template <typename T> inline T dot(const Vector3<T> &v1, const Vector3<T> &v2) {
+	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z; 
+}
+
+template <typename T> inline T absDot(const Vector3<T> &v1, const Vector3<T> &v2) {
+	return std::abs(dot(v1, v2));
+}
+
+// Cross product
+template <typename T> inline Vector3<T> cross(const Vector3<T> &v1, const Vector3<T> &v2) {
+	double v1x = v1.x, v1y = v1.y, v1z = v1.z;
+	double v2x = v2.x, v2y = v2.y, v2z = v2.z;
+	return Vector3<T>((v1y * v2z) - (v1z * v2y), 
+			  (v1z * v2x) - (v1x * v2z),
+			  (v1x * v2y) - (v1y * v2x));
+}
+
+// Compare vectors
+template <typename T> inline bool operator==(const Vector3<T> &v1, const Vector3<T> &v2) {
+	return v1.x == v2.x && v1.y == v2.y && v1.z == v2.z;
+}
+
+template <typename T> inline bool operator!=(const Vector3<T> &v1, const Vector3<T> &v2) {
+	return v1.x != v2.x || v1.y != v2.y || v1.z != v2.z;
+}
+
+typedef Vector3<float> Vector3f;
+typedef Vector3<int> Vector3i;
+
+}
+
+#endif
