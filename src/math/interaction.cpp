6810a2fad9652946186061ee493db97f430a47ff
Georgi Arnaudov <georgearnaudov1@gmail.com>
2021-08-26 21:15:33 +0300

    Apply data encapsulation on interaction entity

--- src/math/interaction.cpp
+++ src/math/interaction.cpp
@@ -3,18 +3,38 @@
 namespace apollo {
 	
 	Interaction::Interaction(const Point3f &p, const Normal3f &n, const Vector3f &wo, float time) 
-				: p(p), n(n), wo(wo), time(time) {}
+				: _p(p), _n(n), _wo(wo), _time(time) {}
+	
+	// Accessor methods
+	const Point3f& Interaction::p() const { return _p; }
+	Point3f& Interaction::p() { return _p; }
+	const float& Interaction::time() const { return _time; }
+	float Interaction::time() { return _time; }
+	const Vector3f& Interaction::wo() const { return _wo; }
+	Vector3f& Interaction::wo() { return _wo; }
+	const Normal3f& Interaction::n() const { return _n; }
+	Normal3f& Interaction::n() { return _n; }
 
 	SurfaceInteraction::SurfaceInteraction(const Point3f &p, const Point2f &uv, const Vector3f &wo,
 		const Vector3f &dpdu, const Vector3f &dpdv,          
 		const Vector3f &dndu, const Vector3f &dndv,       
 		float time, const Shape *shape) : Interaction(p, Normal3f(Cross(dpdu, dpdv)), wo, time),
-		uv(uv), dpdu(dpdu), dpdv(dpdv), dndu(dndu), dndv(dndv), shape(shape) {
+		_uv(uv), _dpdu(dpdu), _dpdv(dpdv), _dndu(dndu), _dndv(dndv), shape(shape) {
 		// Swap normal direction if shape has reverse orientation or 
 		// object to world transformation changes coordinate system handedness
 		if (shape && (shape->reverseOrientation ^ shape->transformChangesHandedness))
-			n *= -1;	
+			_n *= -1;	
 	}
-	
 
+	// Accessor methods
+	const Point2f& SurfaceInteraction::uv() const { return _uv; }
+	Point2f& SurfaceInteraction::uv() { return _uv; }
+	const Vector3f& SurfaceInteraction::dpdu() const { return _dpdu; }	
+	Vector3f& SurfaceInteraction::dpdu() { return _dpdu; }
+	const Vector3f& SurfaceInteraction::dpdv() const { return _dpdv; }	
+	Vector3f& SurfaceInteraction::dpdv() { return _dpdv; }
+	const Normal3f& SurfaceInteraction::dndu() const { return _dndu; }		
+	Normal3f& SurfaceInteraction::dndu() { return _dndu; }
+	const Normal3f& SurfaceInteraction::dndv() const { return _dndv; }	
+	Normal3f& SurfaceInteraction::dndv() { return _dndv; }
 }
--- src/math/interaction.h
+++ src/math/interaction.h
@@ -12,15 +12,26 @@ namespace apollo {
 class Interaction {
 	public:
 		Interaction(const Point3f &p, const Normal3f &n, const Vector3f &wo, float time);
+		
+		// Accessor methods
+		const Point3f& p() const;
+		Point3f& p();
+		const float& time() const;
+		float time();
+		const Vector3f& wo() const;
+		Vector3f& wo();
+		const Normal3f& n() const;
+		Normal3f& n();
+
 	protected:
 		// Intersection point
-		Point3f p;
+		Point3f _p;
 		// Time of intersection
-		float time;
+		float _time;
 		// Negative ray direction
-		Vector3f wo;
+		Vector3f _wo;
 		// Surface normal
-		Normal3f n;
+		Normal3f _n;
 };
 
 // SurfaceInteraction entity represents local information at a point on a surface
@@ -31,12 +42,25 @@ class SurfaceInteraction : public Interaction {
 				   const Vector3f &dndu, const Vector3f &dndv,
 				   float time, const Shape *shape);
 
+		// Accessor methods
+		const Point2f& uv() const;
+		Point2f& uv();
+		const Vector3f& dpdu() const;
+		Vector3f& dpdu();
+		const Vector3f& dpdv() const;
+		Vector3f& dpdv();
+		const Normal3f& dndu() const;
+		Normal3f& dndu();
+		const Normal3f& dndv() const;
+		Normal3f& dndv();
+
 	private:
 		// (u,v) coords from the parameterization of the surface
-		Point2f uv;
+		Point2f _uv;
 		// Parametric partial derivatives of the point and surface normal
-		Vector3f dpdu, dpdv;
-		Normal3f dndu, dndv;
+		Vector3f _dpdu, _dpdv;
+		Normal3f _dndu, _dndv;
+	public:
 		// The shape that the point lies on
 		const Shape *shape = nullptr;
 };
