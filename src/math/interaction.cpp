896d6d4d1e5f8ddcd8e6654afabd08dea5409708
Georgi Arnaudov <georgearnaudov1@gmail.com>
2021-08-26 20:21:10 +0300

    Create surface interaction entity

--- src/math/interaction.cpp
+++ src/math/interaction.cpp
@@ -2,6 +2,19 @@
 
 namespace apollo {
 	
-	Interaction::Interaction(const Point3f &p, const Normal3f &n, const Vector3f &wo, float time) : p(p), n(n), wo(wo), time(time) {}
+	Interaction::Interaction(const Point3f &p, const Normal3f &n, const Vector3f &wo, float time) 
+				: p(p), n(n), wo(wo), time(time) {}
+
+	SurfaceInteraction::SurfaceInteraction(const Point3f &p, const Point2f &uv, const Vector3f &wo,
+		const Vector3f &dpdu, const Vector3f &dpdv,          
+		const Vector3f &dndu, const Vector3f &dndv,       
+		float time, const Shape *shape) : Interaction(p, Normal3f(Cross(dpdu, dpdv)), wo, time),
+		uv(uv), dpdu(dpdu), dpdv(dpdv), dndu(dndu), dndv(dndv), shape(shape) {
+		// Swap normal direction if shape has reverse orientation or 
+		// object to world transformation changes coordinate system handedness
+		if (shape && (shape->reverseOrientation ^ shape->transformChangesHandedness))
+			n *= -1;	
+	}
+	
 
 }
--- src/math/interaction.h
+++ src/math/interaction.h
@@ -2,8 +2,10 @@
 #define APOLLO_MATH_INTERACTION_H
 
 #include "point3.h"
+#include "point2.h"
 #include "normal3.h"
 #include "vector3.h"
+#include "shape.h"
 
 namespace apollo {
 
@@ -11,7 +13,7 @@ class Interaction {
 	public:
 		Interaction(const Point3f &p, const Normal3f &n, const Vector3f &wo, float time);
 	protected:
-		// Intersected point
+		// Intersection point
 		Point3f p;
 		// Time of intersection
 		float time;
@@ -19,9 +21,25 @@ class Interaction {
 		Vector3f wo;
 		// Surface normal
 		Normal3f n;
-		
+};
+
+// SurfaceInteraction entity represents local information at a point on a surface
+class SurfaceInteraction : public Interaction {
+	public:
+		SurfaceInteraction(const Point3f &p, const Point2f &uv, const Vector3f &wo,
+				   const Vector3f &dpdu, const Vector3f &dpdv,
+				   const Vector3f &dndu, const Vector3f &dndv,
+				   float time, const Shape *shape);
+
+	private:
+		// (u,v) coords from the parameterization of the surface
+		Point2f uv;
+		// Parametric partial derivatives of the point and surface normal
+		Vector3f dpdu, dpdv;
+		Normal3f dndu, dndv;
+		// The shape that the point lies on
+		const Shape *shape = nullptr;
 };
 
 }
-
 #endif
